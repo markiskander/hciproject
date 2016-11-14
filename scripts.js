cd5b7f728f1b2c2245ea0dabf382d5c10e9104fb
Mark Iskander <motiontracker@gmail.com>
2016-11-13 20:02:11 -0600

    Completed sign-up page

--- scripts.js
+++ scripts.js
@@ -1,15 +1,40 @@
 function testFunction() {
-    window.alert("JavaScript is fully operational!");
+  window.alert("JavaScript is fully operational!");
 }
 
 function goToSignup() {
-    window.location.href = "signup.html";
+  window.location.href = "signup.html";
 }
 
 function goToMenu() {
-    window.location.href = "menu.html";
+  window.location.href = "menu.html";
 }
 
 function goToLogin() {
-    window.location.href = "login.html";
+  window.location.href = "login.html";
+}
+
+function goToHome() {
+  window.location.href = "index.html";
+}
+
+function completeSignup() {
+  var inputs = document.getElementsByClassName("signup_input");
+  var formFilledOut = true;
+
+  for (var i = 0; i < inputs.length; i++) {
+    if(inputs[i].value == ""){
+      formFilledOut = false;
+    }
+  }
+
+  if(formFilledOut)
+  {
+    window.alert("You have successfully signed up! (Prototypically, that is)");
+    goToHome();
+  }
+  else
+  {
+    window.alert("Please fill out all fields.");
+  }
 }
