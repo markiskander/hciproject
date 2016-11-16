3efb20f27c23989569d8b2907cf66d8eff25fa5a
Mark Iskander <motiontracker@gmail.com>
2016-11-15 23:21:54 -0600

    Massive item data formatting overhaul

--- scripts.js
+++ scripts.js
@@ -1,5 +1,321 @@
 var tabs = ["pasta", "steak", "soup", "beverage", "dessert"];
 var alreadyAlerted = false;
+var currentItem = 0;
+var alreadyCleared = false;
+var priceTotal = 0;
+
+var items = [
+{
+},
+{
+  name:"Fettuccine Alfredo",
+  ingredients1:"Fettuccine Pasta",
+  ingredients2:"Butter",
+  ingredients3:"Salt and Pepper",
+  ingredients4:"2-Cheese Blend",
+  allergens:"None",
+  extra1:"Add chicken",
+  extra2:"Extra cheese",
+  extra3:"Extra sauce"
+},
+{
+  name:"4-Cheese Lasagna",
+  ingredients1:"Lasagna Noodles",
+  ingredients2:"Garlic",
+  ingredients3:"4-Cheese Blend",
+  ingredients4:"Tomato Sauce",
+  allergens:"None",
+  extra1:"Add beef",
+  extra2:"Extra cheese",
+  extra3:"Light cheese"
+},
+{
+  name:"Seafood Linguine",
+  ingredients1:"Linguine Pasta",
+  ingredients2:"Olive Oil",
+  ingredients3:"Shrimp & Crab",
+  ingredients4:"Alfredo Sauce",
+  allergens:"Seafood sensitivity",
+  extra1:"Extra shrimp",
+  extra2:"Add Italian sausage",
+  extra3:"Extra sauce"
+},
+{
+  name:"Spaghetti Meatballs",
+  ingredients1:"Spaghetti Pasta",
+  ingredients2:"Tomato Paste",
+  ingredients3:"Lean Beef",
+  ingredients4:"Black Peppers",
+  allergens:"None",
+  extra1:"Extra meatballs",
+  extra2:"Add sausage",
+  extra3:"Add cheese"
+},
+{
+  name:"Tomato Rotini",
+  ingredients1:"Rotini Pasta",
+  ingredients2:"Tomato Paste",
+  ingredients3:"Garlic",
+  ingredients4:"Parmesan Cheese",
+  allergens:"None",
+  extra1:"Add chicken",
+  extra2:"Add sausage",
+  extra3:"Extra cheese"
+},
+{
+  name:"Beef Ravioli",
+  ingredients1:"Ravioli Pasta",
+  ingredients2:"Lean Beef",
+  ingredients3:"Tomato Paste",
+  ingredients4:"Pepper Mix",
+  allergens:"None",
+  extra1:"Add sausage",
+  extra2:"Add cheese",
+  extra3:"Extra sauce"
+},
+{
+  name:"Top Sirloin",
+  ingredients1:"Top Sirloin Steak (8oz)",
+  ingredients2:"Mashed Potatoes",
+  ingredients3:"Asparagus Spears",
+  ingredients4:"Vegetables",
+  allergens:"None",
+  extra1:"Add mushrooms",
+  extra2:"Upsize (+ 4oz)",
+  extra3:"Extra mashed potatoes"
+},
+{
+  name:"New York Steak",
+  ingredients1:"New York Steak (8oz)",
+  ingredients2:"Baked Potato",
+  ingredients3:"Vegetables",
+  ingredients4:"Pepper Mix",
+  allergens:"None",
+  extra1:"Add mushrooms",
+  extra2:"Upsize (+ 6oz)",
+  extra3:"Extra vegetables"
+},
+{
+  name:"Peppercorn Steak",
+  ingredients1:"Peppercorn Steak (10oz)",
+  ingredients2:"Mashed Potatoes",
+  ingredients3:"Peppercorn Sauce",
+  ingredients4:"Vegetables",
+  allergens:"Peanuts (if selected)",
+  extra1:"Add nut medley",
+  extra2:"Upsize (+ 4oz)",
+  extra3:"Add mushrooms"
+},
+{
+  name:"Sirloin Oscar",
+  ingredients1:"Top Sirloin Steak (8oz)",
+  ingredients2:"Scallops",
+  ingredients3:"Sea Sauce",
+  ingredients4:"Mashed Potatoes",
+  allergens:"Seafood sensitivity",
+  extra1:"Add shrimp",
+  extra2:"Extra sauce",
+  extra3:"Add vegetables"
+},
+{
+  name:"Chicken Gnocci",
+  ingredients1:"Gnocci Dough",
+  ingredients2:"Chicken Broth",
+  ingredients3:"Parsley Flakes",
+  ingredients4:"Chicken",
+  allergens:"None",
+  extra1:"Add cheese",
+  extra2:"Extra chicken",
+  extra3:"No parsley"
+},
+{
+  name:"Chicken Noodles",
+  ingredients1:"Chicken Broth",
+  ingredients2:"Chicken",
+  ingredients3:"Noodle Pasta",
+  ingredients4:"Vegetables",
+  allergens:"None",
+  extra1:"Extra chicken",
+  extra2:"Extra vegetable",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Cream of Broccoli",
+  ingredients1:"Broccoli Cream",
+  ingredients2:"Broccoli",
+  ingredients3:"Onions",
+  ingredients4:"Parmesan Cheese",
+  allergens:"None",
+  extra1:"Add sausage",
+  extra2:"Extra cheese",
+  extra3:"Extra broccoli"
+},
+{
+  name:"Zuppa Toscana",
+  ingredients1:"Italian Sausage",
+  ingredients2:"Potatoes",
+  ingredients3:"Bacon Bits",
+  ingredients4:"Chicken Broth",
+  allergens:"None",
+  extra1:"Extra sausage",
+  extra2:"Extra bacon",
+  extra3:"Add cheese"
+},
+{
+  name:"Spicy Thai",
+  ingredients1:"Spicy Chicken",
+  ingredients2:"Parmesan Cheese",
+  ingredients3:"Creamy Thai Broth",
+  ingredients4:"Vegetables",
+  allergens:"None",
+  extra1:"Extra chicken",
+  extra2:"Extra cheese",
+  extra3:"Extra vegetables"
+},
+{
+  name:"Beef Vegetable",
+  ingredients1:"Beef Broth",
+  ingredients2:"Lean Beef",
+  ingredients3:"Potatoes",
+  ingredients4:"Vegetables",
+  allergens:"None",
+  extra1:"Extra beef",
+  extra2:"Extra vegetables",
+  extra3:"Add bacon"
+},
+{
+  name:"Coca-Cola",
+  ingredients1:"Water",
+  ingredients2:"Sugar",
+  ingredients3:"Food Coloring",
+  ingredients4:"Caffeine",
+  allergens:"Caffeine sensitivity",
+  extra1:"Diet",
+  extra2:"Include straw",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Iced Tea",
+  ingredients1:"Water",
+  ingredients2:"Sugar",
+  ingredients3:"Food Coloring",
+  ingredients4:"Tea Leaves",
+  allergens:"None",
+  extra1:"Diet",
+  extra2:"Include straw",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Ginger Ale",
+  ingredients1:"Water",
+  ingredients2:"Sugar",
+  ingredients3:"Food Coloring",
+  ingredients4:"Ginger",
+  allergens:"None",
+  extra1:"Diet",
+  extra2:"Include straw",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Root Beer",
+  ingredients1:"Water",
+  ingredients2:"Sugar",
+  ingredients3:"Food Coloring",
+  ingredients4:"Vanilla",
+  allergens:"None",
+  extra1:"Diet",
+  extra2:"Include straw",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Coffee",
+  ingredients1:"Water",
+  ingredients2:"Coffee Beans",
+  ingredients3:"Milk (Optional)",
+  ingredients4:"Sugar (Optional)",
+  allergens:"Caffeine sensitivity",
+  extra1:"Decaf",
+  extra2:"Add milk",
+  extra3:"Add sugar"
+},
+{
+  name:"Tea",
+  ingredients1:"Water",
+  ingredients2:"Tea Leaves",
+  ingredients3:"Milk (Optional)",
+  ingredients4:"Sugar (Optional)",
+  allergens:"Caffeine sensitivity",
+  extra1:"Decaf",
+  extra2:"Add milk",
+  extra3:"Add sugar"
+},
+{
+  name:"Swirl Ice Cream",
+  ingredients1:"Vanilla Ice Cream",
+  ingredients2:"Chocolate Ice Cream",
+  ingredients3:"Strawberry Ice Cream",
+  ingredients4:"Sugar",
+  allergens:"Peanuts (if selected)",
+  extra1:"Add nuts",
+  extra2:"Add chocolate sauce",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Chocolate Brownie",
+  ingredients1:"Soft Dough",
+  ingredients2:"Chocolate",
+  ingredients3:"Sugar",
+  ingredients4:"Flour",
+  allergens:"None",
+  extra1:"Add chocolate sauce",
+  extra2:"Add strawberry sauce",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Strawberry Sundae",
+  ingredients1:"Vanilla Ice Cream",
+  ingredients2:"Strawberry Sauce",
+  ingredients3:"Fresh Strawberries",
+  ingredients4:"Sugar",
+  allergens:"None",
+  extra1:"Extra strawberries",
+  extra2:"Extra strawberry sauce",
+  extra3:"Add chocolate sauce"
+},
+{
+  name:"Chocolate Death",
+  ingredients1:"Soft Dough",
+  ingredients2:"Sugar",
+  ingredients3:"Chocolate",
+  ingredients4:"A Lot More Chocolate",
+  allergens:"None",
+  extra1:"Add chocolate sauce",
+  extra2:"Add brownie",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Apple Pie",
+  ingredients1:"Pastry Dough",
+  ingredients2:"Fresh Apples",
+  ingredients3:"Apple Sauce",
+  ingredients4:"Sugar",
+  allergens:"None",
+  extra1:"Extra crispy",
+  extra2:"Add ice cream",
+  extra3:"Upsize (2x)"
+},
+{
+  name:"Strawberry Cheesecake",
+  ingredients1:"Sweet Cream Cheese",
+  ingredients2:"Pastry Dough",
+  ingredients3:"Strawberry Sauce",
+  ingredients4:"Fresh Strawberries",
+  allergens:"None",
+  extra1:"Extra strawberries",
+  extra2:"Add ice cream",
+  extra3:"Add chocolate sauce"
+},
+];
 
 function goToSignup() {
   window.location.href = "signup.html";
@@ -53,201 +369,28 @@ function openItem(item) {
   var allergens = document.getElementsByClassName("item_allergens")[0];
   var extraOptions = document.getElementsByClassName("extra_options")[0];
   var checkbox = "<input type='checkbox'>";
+  var curr = items[item];
 
-  if(item == 1){
-    title.innerHTML = "Fettuccine Alfredo";
-    ingredients.innerHTML = "Fettuccine Pasta<br>Butter<br>Salt and Pepper<br>2-Cheese Blend";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add chicken<br>" + checkbox + "Extra cheese<br>";
-    extraOptions.innerHTML += checkbox + "Extra sauce";
-  }
-  else if(item == 2){
-    title.innerHTML = "4-Cheese Lasagna";
-    ingredients.innerHTML = "Lasagna Noodles<br>Garlic<br>4-Cheese Blend<br>Tomato Sauce";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add beef<br>" + checkbox + "Extra cheese<br>";
-    extraOptions.innerHTML += checkbox + "Light cheese";
-  }
-  else if(item == 3){
-    title.innerHTML = "Seafood Linguine";
-    ingredients.innerHTML = "Linguine Pasta<br>Olive Oil<br>Shrimp & Crab<br>Alfredo Sauce";
-    allergens.innerHTML = "Seafood sensitivity";
-    extraOptions.innerHTML = checkbox + "Extra shrimp<br>" + checkbox + "Add Italian sausage<br>";
-    extraOptions.innerHTML += checkbox + "Extra sauce";
-  }
-  else if(item == 4){
-    title.innerHTML = "Spaghetti  Meatballs";
-    ingredients.innerHTML = "Spaghetti Pasta<br>Tomato Paste<br>Lean Beef<br>Black Peppers";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Extra meatballs<br>" + checkbox + "Add sausage<br>";
-    extraOptions.innerHTML += checkbox + "Add cheese";
-  }
-  else if(item == 5){
-    title.innerHTML = "Tomato Rotini";
-    ingredients.innerHTML = "Rotini Pasta<br>Tomato Paste<br>Garlic<br>Parmesan Cheese";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add chicken<br>" + checkbox + "Add sausage<br>";
-    extraOptions.innerHTML += checkbox + "Extra cheese";
-  }
-  else if(item == 6){
-    title.innerHTML = "Beef Ravioli";
-    ingredients.innerHTML = "Ravioli Pasta<br>Lean Beef<br>Tomato Paste<br>Pepper Mix";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add sausage<br>" + checkbox + "Add cheese<br>";
-    extraOptions.innerHTML += checkbox + "Extra sauce";
-  }
-  else if(item == 7){
-    title.innerHTML = "Top Sirloin";
-    ingredients.innerHTML = "Top Sirloin Steak (8oz)<br>Mashed Potatoes<br>Asparagus Spears<br>Misc. Vegetables";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add mushrooms<br>" + checkbox + "Upsize (+ 4oz)<br>";
-    extraOptions.innerHTML += checkbox + "Extra mashed potatoes";
-  }
-  else if(item == 8){
-    title.innerHTML = "New York Steak";
-    ingredients.innerHTML = "New York Steak (8oz)<br>Baked Potato<br>Misc. Vegetables<br>Pepper Mix";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add mushrooms<br>" + checkbox + "Upsize (+ 6oz)<br>";
-    extraOptions.innerHTML += checkbox + "Extra vegetables";
-  }
-  else if(item == 9){
-    title.innerHTML = "Peppercorn Steak";
-    ingredients.innerHTML = "Peppercorn Steak (10oz)<br>Mashed Potatoes<br>Peppercorn sauce<br>Vegetables";
-    allergens.innerHTML = "Peanuts (if selected)";
-    extraOptions.innerHTML = checkbox + "Add nut medley<br>" + checkbox + "Upsize (+4 oz)<br>";
-    extraOptions.innerHTML += checkbox + "Add mushroom";
-  }
-  else if(item == 10){
-    title.innerHTML = "Sirloin Oscar";
-    ingredients.innerHTML = "Top Sirloin Steak (8oz)<br>Scallops<br>Sea Sauce<br>Mashed Potatoes";
-    allergens.innerHTML = "Seafood sensitivity";
-    extraOptions.innerHTML = checkbox + "Add shrimp<br>" + checkbox + "Extra sauce<br>";
-    extraOptions.innerHTML += checkbox + "Add vegetables";
-  }
-  else if(item == 11){
-    title.innerHTML = "Chicken Gnocci";
-    ingredients.innerHTML = "Gnocci Dough<br>Chicken Broth<br>Parsley Flakes<br>Chicken";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add cheese<br>" + checkbox + "Extra chicken<br>";
-    extraOptions.innerHTML += checkbox + "No parsley";
-  }
-  else if(item == 12){
-    title.innerHTML = "Chicken Noodles";
-    ingredients.innerHTML = "Chicken Broth<br>Chicken<br>Noodle Pasta<br>Vegetables";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Extra chicken<br>" + checkbox + "Extra vegetables<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 13){
-    title.innerHTML = "Cream of Broccoli";
-    ingredients.innerHTML = "Broccoli Cream<br>Broccoli<br>Onions<br>Parmesan Cheese";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add sausage<br>" + checkbox + "Extra cheese<br>";
-    extraOptions.innerHTML += checkbox + "Extra broccoli";
-  }
-  else if(item == 14){
-    title.innerHTML = "Zuppa Toscana";
-    ingredients.innerHTML = "Italian Sausage<br>Potatoes<br>Bacon Bits<br>Chicken Broth";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Extra sausage<br>" + checkbox + "Extra bacon<br>";
-    extraOptions.innerHTML += checkbox + "Add cheese";
-  }
-  else if(item == 15){
-    title.innerHTML = "Spicy Thai";
-    ingredients.innerHTML = "Spicy Chicken<br>Parmesan Cheese<br>Creamy Thai Broth<br>Vegetables";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Extra chicken<br>" + checkbox + "Extra cheese<br>";
-    extraOptions.innerHTML += checkbox + "Extra vegetables";
-  }
-  else if(item == 16){
-    title.innerHTML = "Beef Vegetable";
-    ingredients.innerHTML = "Beef Broth<br>Lean Beef<br>Potatoes<br>Vegetables";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Extra beef<br>" + checkbox + "Extra Vegetables<br>";
-    extraOptions.innerHTML += checkbox + "Add bacon";
-  }
-  else if(item == 17){
-    title.innerHTML = "Coca-Cola";
-    ingredients.innerHTML = "Water<br>Sugar<br>Food Coloring<br>Caffeine";
-    allergens.innerHTML = "Caffeine sensitivity";
-    extraOptions.innerHTML = checkbox + "Diet<br>" + checkbox + "Include straw<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 18){
-    title.innerHTML = "Iced Tea";
-    ingredients.innerHTML = "Water<br>Sugar<br>Food Coloring<br>Tea Leaves";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Diet<br>" + checkbox + "Include straw<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 19){
-    title.innerHTML = "Ginger Ale";
-    ingredients.innerHTML = "Water<br>Sugar<br>Food Coloring<br>Ginger";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Diet<br>" + checkbox + "Include straw<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 20){
-    title.innerHTML = "Root Beer";
-    ingredients.innerHTML = "Water<br>Sugar<br>Food Coloring<br><br>";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Diet<br>" + checkbox + "Include straw<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 21){
-    title.innerHTML = "Coffee";
-    ingredients.innerHTML = "Water<br>Coffee Beans<br><br><br>";
-    allergens.innerHTML = "Caffeine sensitivity";
-    extraOptions.innerHTML = checkbox + "Decaf<br>" + "<br><br>";
-  }
-  else if(item == 22){
-    title.innerHTML = "Tea";
-    ingredients.innerHTML = "Water<br>Tea Leaves<br><br><br>";
-    allergens.innerHTML = "Caffeine sensitivity";
-    extraOptions.innerHTML = checkbox + "Decaf<br>" + "<br><br>";
-  }
-  else if(item == 23){
-    title.innerHTML = "Swirl Ice Cream";
-    ingredients.innerHTML = "Vanilla Ice Cream<br>Chocolate Ice Cream<br>Strawberry Ice Cream<br>Sugar";
-    allergens.innerHTML = "Peanuts (if selected)";
-    extraOptions.innerHTML = checkbox + "Add nuts<br>" + checkbox + "Add chocolate sauce<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 24){
-    title.innerHTML = "Chocolate Brownie";
-    ingredients.innerHTML = "Soft Dough<br>Chocolate<br>Sugar<br>Flour";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add chocolate sauce<br>" + checkbox + "Add strawberry sauce<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 25){
-    title.innerHTML = "Strawberry Sundae";
-    ingredients.innerHTML = "Vanilla Ice Cream<br>Strawberry Sauce<br>Fresh Strawberries<br>Sugar";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Extra strawberries<br>" + checkbox + "Extra sauce<br>";
-    extraOptions.innerHTML += checkbox + "Add chocolate sauce";
-  }
-  else if(item == 26){
-    title.innerHTML = "Chocolate Death";
-    ingredients.innerHTML = "Soft Dough<br>Sugar<br>Chocolate<br>Much More Chocolate";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Add chocolate sauce<br>" + checkbox + "Add brownie<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 27){
-    title.innerHTML = "Apple Pie";
-    ingredients.innerHTML = "Pastry Dough<br>Fresh Apples<br>Apple Sauce<br>Sugar";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Extra crispy<br>" + checkbox + "Add ice cream<br>";
-    extraOptions.innerHTML += checkbox + "Upsize (2x)";
-  }
-  else if(item == 28){
-    title.innerHTML = "Strawberry Cheesecake";
-    ingredients.innerHTML = "Sweet Cream Cheese<br>Pastry Dough<br>Strawberry Sauce<br>Fresh Strawberries";
-    allergens.innerHTML = "None";
-    extraOptions.innerHTML = checkbox + "Extra sausage<br>" + checkbox + "Extra bacon<br>";
-    extraOptions.innerHTML += checkbox + "Add cheese";
-  }
+  currentItem = item;
+
+  title.innerHTML = "";
+  ingredients.innerHTML = "";
+  allergens.innerHTML = "";
+  extraOptions.innerHTML = "";
+
+
+  title.innerHTML = curr.name;
+
+  ingredients.innerHTML += curr.ingredients1;
+  ingredients.innerHTML += "<br>" + curr.ingredients2;
+  ingredients.innerHTML += "<br>" + curr.ingredients3;
+  ingredients.innerHTML += "<br>" + curr.ingredients4;
+
+  allergens.innerHTML = curr.allergens;
+
+  extraOptions.innerHTML += checkbox + curr.extra1;
+  extraOptions.innerHTML += "<br>" + checkbox + curr.extra2;
+  extraOptions.innerHTML += "<br>" + checkbox + curr.extra3;
 }
 
 function closeOverlay() {
@@ -255,6 +398,19 @@ function closeOverlay() {
 }
 
 function submitPress() {
+  var orderItems = document.getElementById("order_items");
+
+  if(alreadyCleared == false){
+    orderItems.innerHTML = "";
+    alreadyCleared = true;
+  }
+
+  if(currentItem == 1){
+    orderItems.innerHTML += "Fettuccine Alfredo - 14.99<br><br>";
+    priceTotal += 14.99;
+  }
+
+
   closeOverlay();
 }
 
